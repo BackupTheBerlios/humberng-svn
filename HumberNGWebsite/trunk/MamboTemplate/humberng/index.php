2f7f0ea51c5323bbb193b715cdc51588117d52d5
cyberroadie <cyberroadie@830a7435-c8ff-0310-94f7-8779cb1a3b5d>
2005-09-12 11:07:04 +0000

    IE fix for PNG

--- HumberNGWebsite/trunk/MamboTemplate/humberng/index.php
+++ HumberNGWebsite/trunk/MamboTemplate/humberng/index.php
@@ -6,35 +6,43 @@ echo '<?xml version="1.0" encoding="'. $iso[1] .'"?' .'>';
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-	<!--[if gte IE 5.5000]>
-	<script language="JavaScript">
-	function correctPNG() // correctly handle PNG transparency in Win IE 5.5 or higher.
-	   {   
-	   for(var i=0; i<document.images.length; i++)
-	      {
-	          var img = document.images[i]
-	          var imgName = img.src.toUpperCase()
-	          if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
-	             {
-	                 var imgID = (img.id) ? "id='" + img.id + "' " : ""
-	                 var imgClass = (img.className) ? "class='" + img.className + "' " : ""
-	                 var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
-	                 var imgStyle = "display:inline-block;" + img.style.cssText
-	                 if (img.align == "left") imgStyle = "float:left;" + imgStyle
-	                 if (img.align == "right") imgStyle = "float:right;" + imgStyle
-	                 if (img.parentElement.href) imgStyle = "cursor:hand;" + imgStyle              
-	                 var strNewHTML = "<span " + imgID + imgClass + imgTitle
-	                 + " style="" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
-	             + "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
-	                 + "(src='" + img.src + "', sizingMethod='scale');"></span>"
-	                 img.outerHTML = strNewHTML
-	                 i = i-1
-	             }
-	      }
-	   }
-	window.attachEvent("onload", correctPNG);
-	</script>
-	<![endif]-->    
+<!--[if gte IE 5.5000]>
+<script language="JavaScript">
+function correctPNG() // correctly handle PNG transparency in Win IE 5.5 or higher.
+{
+for(var i=0; i<document.images.length; i++)
+{
+var img = document.images[i]
+var imgName = img.src.toUpperCase()
+if (imgName.substring(imgName.length-3, imgName.length) == "PNG")
+{
+var imgID = (img.id) ? "id='" + img.id + "' " : ""
+var imgClass = (img.className) ? "class='" + img.className + "' " : ""
+var imgTitle = (img.title) ? "title='" + img.title + "' " : "title='" + img.alt + "' "
+var imgStyle = "display:inline-block;" + img.style.cssText
+var imgAttribs = img.attributes;
+for (var j=0; j<imgAttribs.length; j++)
+{
+var imgAttrib = imgAttribs[j];
+if (imgAttrib.nodeName == "align")
+{
+if (imgAttrib.nodeValue == "left") imgStyle = "float:left;" + imgStyle
+if (imgAttrib.nodeValue == "right") imgStyle = "float:right;" + imgStyle
+break
+}
+}
+var strNewHTML = "<span " + imgID + imgClass + imgTitle
+strNewHTML += " style=\"" + "width:" + img.width + "px; height:" + img.height + "px;" + imgStyle + ";"
+strNewHTML += "filter:progid:DXImageTransform.Microsoft.AlphaImageLoader"
+strNewHTML += "(src=\'" + img.src + "\', sizingMethod='scale');\"></span>"
+img.outerHTML = strNewHTML
+i = i-1
+}
+}
+}
+window.attachEvent("onload", correctPNG);
+</script>
+<![endif]-->   
     <?php if ( $my->id ) initEditor(); ?>
     <meta http-equiv="Content-Type" content="text/html; <?php echo _ISO; ?>" />
     <?php mosShowHead(); ?>
