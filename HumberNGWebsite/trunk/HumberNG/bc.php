f45c377e3a106845cd776220b38399bd56d88cc8
cyberroadie <cyberroadie@830a7435-c8ff-0310-94f7-8779cb1a3b5d>
2005-09-26 15:47:30 +0000

    Initial version

---
+++ HumberNGWebsite/trunk/HumberNG/bc.php
@@ -0,0 +1,14 @@
+<html>
+<body bgcolor="#eeeeee">
+<div align="center">
+<img src="images/humberng.png">
+<?php
+$lines = file("http://dragon3d.berlios.de/ip.txt");
+$ip = $lines[0];
+?>
+</div>
+<div align="center">
+<a href="http://<?php echo $ip ?>/mediawiki/">Broadband Capital Wiki</a>
+</div>
+</body>
+<html>
