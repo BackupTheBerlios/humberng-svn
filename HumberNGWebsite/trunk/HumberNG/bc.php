ec7005db2f0067d541417993a20fb422b48525a4
jtijnagel <jtijnagel@830a7435-c8ff-0310-94f7-8779cb1a3b5d>
2005-09-28 16:49:58 +0000

    spelling problem

--- HumberNGWebsite/trunk/HumberNG/bc.php
+++ HumberNGWebsite/trunk/HumberNG/bc.php
@@ -3,7 +3,7 @@
 	$ip = $lines[0];
 ?><html>
 <head>
-	<title>HuberNG Backend Central</title>
+	<title>HumberNG Backend Central</title>
 </head>
 <body bgcolor="#eeeeee">
 <table width="100%" height="100%"  border="0" cellpadding="0" cellspacing="0">
