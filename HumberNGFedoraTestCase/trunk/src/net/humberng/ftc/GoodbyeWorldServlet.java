09d7ea6ef4bb9a6f36c1f39ca9a2e3cc92630948
cyberroadie <cyberroadie@830a7435-c8ff-0310-94f7-8779cb1a3b5d>
2005-10-07 11:46:40 +0000

    Initial test

---
+++ HumberNGFedoraTestCase/trunk/src/net/humberng/ftc/GoodbyeWorldServlet.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package net.humberng.ftc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author cyberroadie
+ *
+ */
+public class GoodbyeWorldServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+    throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<body>" +
+				    "Goodbye World!! (Servlet)" +
+				    "</body>" +
+				    "</html>");
+	}
+	
+}
